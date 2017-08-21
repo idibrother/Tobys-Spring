db949aea5879e3a43f04da1ea0bade4b96caa2b4
dongba-PC\dongba <idibro@naver.com>
2017-08-21 22:51:22 +0900

    update object-dependency. - add comment.

--- object-dependency/src/test/java/com/idibros/study/dao/UserDaoTest.java
+++ object-dependency/src/test/java/com/idibros/study/dao/UserDaoTest.java
@@ -19,15 +19,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class UserDaoTest {
 
-    private UserDao userDao;
+    private static UserDao userDao;
 
+    // h2 DB의 Driver를 클래스로더에 추가함.
     @BeforeClass
     public static void init() throws ClassNotFoundException {
         Class.forName("org.h2.Driver");
+        userDao = new UserDao();
     }
 
+    // 유닛 테스트마다 상호 독립적으로 수행 할 수 있도록 각 테스트를 실행 할 때 마다 테이블을 제거하고 새로 만듦.
     @Before
     public void setUp() throws ClassNotFoundException, SQLException {
+        // table 생성
         try(Connection conn = DriverManager.getConnection("jdbc:h2:~/object-dependency", "", "");
             PreparedStatement ps = conn.prepareStatement("create table users(id varchar(10) primary key," +
                     " name varchar(20) not null," +
@@ -35,7 +39,6 @@ public class UserDaoTest {
             ps.executeUpdate();
         }
 
-        userDao = new UserDao();
     }
 
     @Test
@@ -65,6 +68,7 @@ public class UserDaoTest {
 
     @After
     public void tearDown() throws SQLException {
+        // 테이블 제거
         try(Connection conn = DriverManager.getConnection("jdbc:h2:~/object-dependency", "", "");
             PreparedStatement ps = conn.prepareStatement("drop table users")) {
             ps.executeUpdate();

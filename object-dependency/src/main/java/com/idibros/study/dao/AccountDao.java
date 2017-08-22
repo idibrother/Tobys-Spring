0a4d11f6db64b0ec0a267bcb1fe98d8d98a7dfd0
dongba-PC\dongba <idibro@naver.com>
2017-08-22 21:14:57 +0900

    update object-dependency - to add other DAO for objects

---
+++ object-dependency/src/main/java/com/idibros/study/dao/AccountDao.java
@@ -0,0 +1,9 @@
+package com.idibros.study.dao;
+
+/**
+ * Created by dongba on 2017-08-22.
+ */
+public class AccountDao {
+    public AccountDao(ConnectionMaker connectionMaker) {
+    }
+}
--- object-dependency/src/main/java/com/idibros/study/dao/DaoFactory.java
+++ object-dependency/src/main/java/com/idibros/study/dao/DaoFactory.java
@@ -6,9 +6,18 @@ package com.idibros.study.dao;
 public class DaoFactory {
 
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new UserDao(connectionMaker());
         return userDao;
+    }
+
+    public AccountDao accountDao() {
+        AccountDao accountDao = new AccountDao(connectionMaker());
+        return accountDao;
+    }
+
+    // 동일한 connectionMaker를 사용하는 DAO의 종류가 여러 개일 경우를 고려해서 connectionMaker를 생성하는 별도의 함수를 정의하였다.
+    public ConnectionMaker connectionMaker() {
+        return new NConnectionMaker();
     }
 
 }

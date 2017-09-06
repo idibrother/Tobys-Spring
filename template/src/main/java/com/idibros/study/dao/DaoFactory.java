2a35408b70775bb5244b2c604c355b06b0f621b1
dongba-PC\dongba <idibro@naver.com>
2017-09-06 23:40:30 +0900

    init template.

--- template/src/main/java/com/idibros/study/dao/DaoFactory.java
+++ template/src/main/java/com/idibros/study/dao/DaoFactory.java
@@ -20,12 +20,6 @@ public class DaoFactory {
     }
 
     @Bean
-    public AccountDao accountDao() {
-        AccountDao accountDao = new AccountDao(dataSource());
-        return accountDao;
-    }
-
-    @Bean
     // 동일한 connectionMaker를 사용하는 DAO의 종류가 여러 개일 경우를 고려해서 connectionMaker를 생성하는 별도의 함수를 정의하였다.
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
